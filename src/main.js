7b58542d1f17a1a8e18a75f7f75f2efcca504be6
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-03-25 19:36:40 +0100

    initial

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 import "@/assets/styles/index.css";
-
+import "@fortawesome/fontawesome-free/css/all.min.css";
 const app = createApp(App);
 window.app = app;
 app.mount("#app");
