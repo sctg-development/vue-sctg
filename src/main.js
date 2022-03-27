ca94b98fc1cd5b0860a48f349b99649ebadb960b
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-03-27 19:09:21 +0200

    start i18n

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
+import { createWebHistory, createRouter } from "vue-router";
+
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+    locale: 'en',
+    messages:{
+        en: require('@/locales/en.json'),
+        fr: require('@/locales/fr.json'),
+    }
+  })
+  const routes = [
+    {
+        path: "/",
+        component: () => import("@/components/IndexPage.vue"),
+        name: 'index',
+      }
+    ];
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+
 import "@/assets/styles/index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 const app = createApp(App);
 window.app = app;
+app.use(i18n).use(router);
 app.mount("#app");
