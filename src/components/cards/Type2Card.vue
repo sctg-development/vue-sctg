6acc79fa5da271fed0e6bbb9cbeb284901136a3b
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-03-29 16:53:28 +0200

    wip

--- src/components/cards/Type2Card.vue
+++ src/components/cards/Type2Card.vue
@@ -3,9 +3,10 @@
     <div class="mb-10 rounded-xl border border-slate-600 bg-slate-700 p-[10px]">
       <div class="relative mb-5 overflow-hidden rounded-lg">
         <img
-          src="https://nft-tailwind.preview.uideck.com/images/auctions/image-01.svg"
+          :src="imgurl"
           alt="auctions"
           class="w-full"
+          @click="lightBox(lightboxUrl)"
         />
         <button
           class="
@@ -21,19 +22,10 @@
             py-1
           "
         >
-          <svg
-            width="14"
-            height="14"
-            viewBox="0 0 14 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z"
-              fill="#F85486"
-            ></path>
-          </svg>
-          <span class="pl-1 text-xs font-semibold text-zinc-900"> 4.6K </span>
+          <img :src="require('@/assets/img/heart.svg')" />
+          <span class="pl-1 text-xs font-semibold text-zinc-900">
+            {{ value }}
+          </span>
         </button>
       </div>
       <div>
@@ -57,7 +49,7 @@
             <div class="flex items-center">
               <div class="mr-2 h-8 w-full max-w-[32px] rounded-md">
                 <img
-                  src="https://nft-tailwind.preview.uideck.com/images/auctions/creator-01.png"
+                  :src="miniImgurl"
                   alt="creator"
                   class="h-full w-full object-cover object-center"
                 />
@@ -66,7 +58,7 @@
                 <h4 class="text-xs font-semibold text-white">
                   {{ author }}
                   <span class="block text-xs font-medium text-zinc-400">
-                    creator
+                    Team
                   </span>
                 </h4>
               </div>
@@ -74,10 +66,8 @@
           </div>
           <div class="w-full">
             <h5 class="text-right text-xs font-semibold text-white">
-              {{ price }}
-              <span class="block text-xs font-medium text-zinc-400">
-                Current Bit
-              </span>
+              {{ authorTitle }}
+              <span class="block text-xs font-medium text-zinc-400"> … </span>
             </h5>
           </div>
         </div>
@@ -87,28 +77,41 @@
             items-center
             justify-center
             rounded-md
-            bg-slate-600 
+            bg-slate-600
             p-[10px]
             text-base
             font-semibold
             text-white
           "
         >
-          {{ time }}
+          {{ baseMessage }}
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import * as basiclightbox from "basiclightbox";
 export default {
   props: {
     title: String,
     imgurl: String,
+    miniImgurl: String,
+    lightboxUrl: String,
     author: String,
-    price: String,
-    time: String,
+    authorTitle: String,
+    baseMessage: String,
+    value: String,
   },
   setup() {},
+  methods: {
+    lightBox: (url) => {
+      if (url !== undefined) {
+        basiclightbox
+          .create(`<img src="${url}" />`)
+          .show(() => console.log(`lightbox ${url} now visible`));
+      }
+    },
+  },
 };
 </script>
