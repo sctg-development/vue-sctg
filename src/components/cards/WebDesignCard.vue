1bbd6b163ddd264e8b8ac44bee9424e57273bce3
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-03-29 14:27:28 +0200

    finish big card 1

--- src/components/cards/WebDesignCard.vue
+++ src/components/cards/WebDesignCard.vue
@@ -58,32 +58,21 @@
       <button
         class="inline-flex font-bold items-center rounded-md bg-white px-2 py-1"
       >
-        <svg
-          width="14"
-          height="14"
-          viewBox="0 0 14 14"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z"
-            fill="#F85486"
-          ></path>
-        </svg>
+        <img :src="require('@/assets/img/heart.svg')"/>
         <span class="pl-1 text-xs font-semibold text-zinc-900"> 1.6K </span>
       </button>
     </div>
     <div class="flex items-center">
       <div class="mr-3 h-[46px] w-full max-w-[46px] overflow-hidden rounded-md">
         <img
-          src="https://nft-tailwind.preview.uideck.com/images/collections/creator-01.jpg"
-          alt=""
+         :src="getCloudinaryImg('web-design-tools', 50).toURL()"
+          alt="Web design tools"
           class="h-full w-full object-cover object-center"
         />
       </div>
       <div class="w-full">
         <h4 class="text-base font-semibold text-white">
-          @Lathium_Lui...
+          webdesign@…
           <span class="block text-sm font-medium text-zinc-400"> Creator </span>
         </h4>
       </div>
