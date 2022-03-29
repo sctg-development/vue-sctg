e866afe29845ee3d0d02b248e71a038e544ebd8d
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-03-29 18:16:43 +0200

    working slider with lightbox

--- src/main.js
+++ src/main.js
@@ -5,27 +5,36 @@ import { createWebHistory, createRouter } from "vue-router";
 import { createI18n } from 'vue-i18n'
 
 const i18n = createI18n({
-    locale: 'en',
-    messages:{
-        en: require('@/locales/en.json'),
-        fr: require('@/locales/fr.json'),
-    }
-  })
-  const routes = [
-    {
-        path: "/",
-        component: () => import("@/components/IndexPage.vue"),
-        name: 'index',
-      }
-    ];
-  const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
+  locale: 'en',
+  messages: {
+    en: require('@/locales/en.json'),
+    fr: require('@/locales/fr.json'),
+  }
+})
+const routes = [
+  {
+    path: "/",
+    component: () => import("@/components/IndexPage.vue"),
+    name: 'index',
+  }
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
 import "@/assets/styles/index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import * as basiclightbox from "basiclightbox";
+
 const app = createApp(App);
 window.app = app;
 app.use(i18n).use(router);
 app.mount("#app");
+app.config.globalProperties.$lightbox = (url) => {
+  if (url !== undefined) {
+    basiclightbox
+      .create(`<img src="${url}" />`)
+      .show(() => console.log(`global $lightbox ${url} now visible`));
+  }
+};
