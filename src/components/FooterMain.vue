303cdaca2fb62ccd989545b20db7040c50493f9c
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-03-30 15:08:39 +0200

    footer color

--- src/components/FooterMain.vue
+++ src/components/FooterMain.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-slate-700 pt-24 inline-block w-full mt-8 bottom-0 right-0">
+  <footer class="bg-slate-800 pt-24 inline-block w-full mt-8 bottom-0 right-0">
     <div
       class="
         w-full
