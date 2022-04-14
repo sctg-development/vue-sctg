e0955e094f5c597744758150845f6d8cfa5f3785
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-04-14 17:12:50 +0200

    BUG: shortlinks with http://sctg.cf/![short]

--- functions/![link].js
+++ functions/![link].js
@@ -3,32 +3,24 @@ export async function onRequestPost(context) {
 }
 
 export async function onRequestGet(context) {
- // await context.env.CfUrlShortener.put('OwljL','https://drive.google.com/file/d/1rSv90taXFftWIN4Aeyqh5tDEheaNUCCK/view');
- let originUrl = (new URL(context.request.url)).origin;
-    if (context.params !== undefined){
-        if (context.params.link !== undefined){
-            let req = context.params.link;
-            let link = await context.env.CfUrlShortener.get(req);
-            if (link) {
-              return new Response(null, {
-                headers: { Location: link },
-                status: 301,
-              });
-            }
-            else
-            {
-              return new Response(null, {
-                headers: { Location: originUrl },
-                status: 301,
-              });
-            }
-            // console.log(`${context.env} ${req} ${link}`);
-            // return new Response(JSON.stringify(link,null,2), {
-            //     headers: {
-            //       "content-type": "application/json;charset=UTF-8"
-            //     }
-            //   });
-        }
+  let originUrl = (new URL(context.request.url)).origin;
+  if (context.params !== undefined) {
+    if (context.params.link !== undefined) {
+      let req = context.params.link;
+      let link = await context.env.SHORTURL.get(req);
+      if (link) {
+        return new Response(null, {
+          headers: { Location: link },
+          status: 301,
+        });
+      }
+      else {
+        return new Response(null, {
+          headers: { Location: originUrl },
+          status: 301,
+        });
+      }
     }
-    return new Response("ERROR NO SHORT LINK PROVIDED",null,2);
-    }
+  }
+  return new Response("ERROR NO SHORT LINK PROVIDED", null, 2);
+}
