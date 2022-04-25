717391683d86e690f80616aac7d9e6dd36057da8
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-04-25 20:27:24 +0200

    retrieve jwks.json async

--- vue.config.js
+++ vue.config.js
@@ -38,35 +38,47 @@ fs.writeFile('./auth0-conf.json',
   }
 );
 
-function listDir(dir){fs.readdir(dir, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
+function listDir(dir) {
+  fs.readdir(dir, (err, files) => {
+      files.forEach(file => {
+          console.log(file);
+      });
   });
-});}
+}
 
-/* retrieve https://sctg.eu.auth0.com/.well-known/jwks.json */
-console.log(`retrieve https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`)
-const https = require('https')
-const url = `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`;
-https.get(url, res => {
-  let data = '';
-  res.on('data', chunk => {
-    data += chunk;
+
+async function getJwks() {
+  console.log(`retrieve https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`)
+  const https = require('https')
+  const url = `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`;
+  return new Promise((resolve, reject) => {
+      https.get(url, res => {
+          let data = '';
+          res.on('data', chunk => {
+              data += chunk;
+          });
+          res.on('end', () => {
+              data = JSON.parse(data);
+              data.domain = process.env.AUTH0_DOMAIN;
+              resolve(data);
+          })
+      }).on('error', err => {
+          console.log(err.message);
+          reject(err);
+      })
   });
-  res.on('end', () => {
-    data = JSON.parse(data);
-    data.domain = process.env.AUTH0_DOMAIN;
-    fs.writeFile('./jwks.json',
-      JSON.stringify(data),
-      'utf8', function (err) {
-        listDir('.');
-        if (err) return console.log(err);
-      }
-    );
-  })
-}).on('error', err => {
-  console.log(err.message);
-})
+}
+
+(async () => {
+  const jwks = await getJwks();
+  fs.writeFile('./jwks.json',
+  JSON.stringify(jwks),
+  'utf8', function (err) {
+      listDir('.');
+      if (err) return console.log(err);
+  });
+})();
+
 
 /*build sanity-conf.json */
 const sanityApiVersion = "2021-10-21";
