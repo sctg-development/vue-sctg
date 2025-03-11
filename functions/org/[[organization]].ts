ad9d5452a7410d842b87534f9c64da8ce281e31a
æLtorio <aeltorio@universidadatlantico.org>
2025-03-11 19:37:53 +0100

    refactor: update GitHub stats fetching function to return detailed statistics

--- functions/org/[[organization]].ts
+++ functions/org/[[organization]].ts
@@ -1,6 +1,12 @@
 import { PagesFunction, EventContext } from "@cloudflare/workers-types";
-const axios = require('axios');
+import axios from 'axios';
 
+type Stats = {
+    nbStars: number;
+    nbForks: number;
+    nbWatchers: number;
+    nbRepos: number;
+}
 /**
  * Function to fetch GitHub api at https://api.github.com/orgs/{organization}/repos
  * which answers a json response in a array of objects
@@ -8,10 +14,10 @@ const axios = require('axios');
  * - id: number
  * - stargazers_count: number
  * @param organization
- * @returns the sum of stargazers_count
+ * @returns the stats of the organization
  */
-async function getTotalStargazersForOrg(organization: string
-):Promise<number> {
+async function getGlobalStatsForOrg(organization: string
+):Promise<Stats> {
     const url = `https://api.github.com/orgs/${organization}/repos?per_page=100`;
     const response = await axios.get(url, {
       headers: {
@@ -20,20 +26,34 @@ async function getTotalStargazersForOrg(organization: string
       }
     });
     const data = response.data;
-    return data.reduce((acc: number, repo: any) => acc + repo.stargazers_count, 0);
+    const stargazers_count= data.reduce((acc: number, repo: any) => acc + repo.stargazers_count, 0);
+    const forks_count = data.reduce((acc: number, repo: any) => acc + repo.forks_count, 0);
+    const watchers_count = data.reduce((acc: number, repo: any) => acc + repo.watchers_count, 0);
+    return {
+        nbStars: stargazers_count,
+        nbForks: forks_count,
+        nbWatchers: watchers_count,
+        nbRepos: data.length
+    };
 }
 
+/**
+ * Cloudflare Pages function to get the stats of an organization
+ * the path is /org/{organization}/stats
+ * @param { params }: EventContext<any, "organization", any> 
+ * @returns 
+ */
 export const onRequestGet = async ({ params }: EventContext<any, "organization", any>) => {
     if (params.organization && params.organization.length === 2) {
         const organization = params.organization[0];
         const method = params.organization[1];
         if (organization === "highcanfly-club" || organization === "sctg-development") {
-            if (method === "stars") {
-                const sum = await getTotalStargazersForOrg(organization);
-                return new Response(`The sum of stargazers_count is ${sum}`, { status: 200 });
+            if (method === "stats") {
+                const stats = await getGlobalStatsForOrg(organization);
+                return new Response(JSON.stringify(stats), { status: 200 , headers: { 'Content-Type': 'application/json' }});
             }
-            return new Response("Hello", { status: 200 });
+            return new Response(JSON.stringify({status:"Unsupported"}), { status: 200, headers: { 'Content-Type': 'application/json' } });
         }
     }
-    return new Response("Error", { status: 500 });
+    return new Response(JSON.stringify({status:"Error"}), { status: 500 , headers: { 'Content-Type': 'application/json' }});
 } 
