ae535fb8aab08495d5d8caf619cb617cf28cae1e
æLtorio <aeltorio@universidadatlantico.org>
2025-03-11 15:07:19 +0100

    wip porting to tailwindcss 4

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ import type { Auth0Instance, RedirectCallback } from "@/auth0";
 import { createI18n } from 'vue-i18n'
 import { createPinia } from "pinia";
 import enLang from '@/locales/en.json'
+import "./assets/styles/index.scss";
 import "./assets/styles/index.css";
 import '@highcanfly-club/fontawesome/styles/fontawesome.css';
 
-import "./assets/styles/index.scss";
+
 
 type MessageSchema = typeof enLang;
 export type AvailableLanguage = 'en' | 'fr' | 'es';
